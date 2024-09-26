036be07dffd6a73c82f85c1ed8c12519fd8719cd
Yolanda WANG <118796571+Ning-2004@users.noreply.github.com>
2024-09-26 23:11:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.decomposition import LatentDirichletAllocation as LDA
 from sklearn.feature_extraction.text import CountVectorizer
 import nltk
